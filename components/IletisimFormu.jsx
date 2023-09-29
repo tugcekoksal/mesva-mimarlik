4699fff8b6a8ca01458f205c64f776c8fc6f5ddc
tugcekoksal <tugcekoksal35@gmail.com>
2023-09-29 17:01:26 +0200

    change

--- components/IletisimFormu.jsx
+++ components/IletisimFormu.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
+import emailjs from "emailjs-com";
+
 function IletisimFormu() {
+  const [statusMessage, setStatusMessage] = useState("");
   const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    message: "",
+    user_name: "",
+    user_email: "",
+    user_message: "",
   });
 
   const handleSubmit = async (e) => {
@@ -16,11 +19,12 @@ function IletisimFormu() {
         "ajNw8hHBtWDc-5DQT"
       );
       if (result.text === "OK") {
-        console.log("E-posta başarıyla gönderildi.");
+        setStatusMessage("E-posta başarıyla gönderildi.");
       } else {
-        console.error("E-posta gönderilirken bir hata oluştu.");
+        setStatusMessage("E-posta gönderilirken bir hata oluştu.");
       }
     } catch (error) {
+      setStatusMessage("E-posta gönderilirken bir hata oluştu.");
       console.error("E-posta gönderilirken bir hata oluştu:", error);
     }
   };
@@ -31,53 +35,60 @@ function IletisimFormu() {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="w-full mx-auto p-4 space-y-4">
-      <div className="flex flex-col">
-        <label htmlFor="name" className="mb-2 font-semibold text-gray-700">
-          Adınız:
-        </label>
-        <input
-          type="text"
-          id="name"
-          name="user_name"
-          value={formData.name}
-          onChange={handleChange}
-          className="p-2 border rounded-md"
-        />
-      </div>
-      <div className="flex flex-col">
-        <label htmlFor="email" className="mb-2 font-semibold text-gray-700">
-          E-posta:
-        </label>
-        <input
-          type="email"
-          id="email"
-          name="user_email"
-          value={formData.email}
-          onChange={handleChange}
-          className="p-2 border rounded-md"
-        />
-      </div>
-      <div className="flex flex-col">
-        <label htmlFor="message" className="mb-2 font-semibold text-gray-700">
-          Mesajınız:
-        </label>
-        <textarea
-          id="message"
-          name="user_message"
-          value={formData.message}
-          onChange={handleChange}
-          className="p-2 border rounded-md"
-          rows="4"
-        ></textarea>
-      </div>
-      <button
-        type="submit"
-        className="w-full p-2 font-semibold bg-slate-500 text-white rounded-md hover:bg-slate-600 transition-colors duration-300"
-      >
-        Gönder
-      </button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit} className="w-full mx-auto p-4 space-y-4">
+        <div className="flex flex-col">
+          <label htmlFor="name" className="mb-2 font-semibold text-gray-700">
+            Adınız:
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="user_name"
+            value={formData.name}
+            onChange={handleChange}
+            className="p-2 border rounded-md"
+          />
+        </div>
+        <div className="flex flex-col">
+          <label htmlFor="email" className="mb-2 font-semibold text-gray-700">
+            E-posta:
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="user_email"
+            value={formData.email}
+            onChange={handleChange}
+            className="p-2 border rounded-md"
+          />
+        </div>
+        <div className="flex flex-col">
+          <label htmlFor="message" className="mb-2 font-semibold text-gray-700">
+            Mesajınız:
+          </label>
+          <textarea
+            id="message"
+            name="user_message"
+            value={formData.message}
+            onChange={handleChange}
+            className="p-2 border rounded-md"
+            rows="4"
+          ></textarea>
+        </div>
+        <button
+          type="submit"
+          className="w-full p-2 font-semibold bg-slate-500 text-white rounded-md hover:bg-slate-600 transition-colors duration-300"
+        >
+          Gönder
+        </button>
+      </form>
+      {statusMessage && ( // Durum mesajını göster
+        <div className="mt-4 p-4 bg-blue-100 border border-blue-400 text-blue-700 rounded-md">
+          {statusMessage}
+        </div>
+      )}
+    </div>
   );
 }
 
