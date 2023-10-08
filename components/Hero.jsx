59a092214b4550f770a2c983f669fda976604bc7
tugcekoksal <tugcekoksal35@gmail.com>
2023-10-08 11:14:51 +0200

    change

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = ({ heading, message }) => {
   }, []);
 
   return (
-    <div className="flex items-center justify-center h-screen mb-12 bg-fixed  bg-center sm:bg-cover custom-img animate">
+    <div className="flex items-center justify-center h-screen mb-12 bg-fixed  custom-img animate">
       {/*overlay*/}
       <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/60 z-[2]" />
       <div className="p-5 text-white z-[2]  mt-[-10rem] ">
