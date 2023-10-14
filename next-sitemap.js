64c641d89cd236a0fcd4012ef691a5b201b12199
tugcekoksal <tugcekoksal35@gmail.com>
2023-10-14 16:20:02 +0200

    change

---
+++ next-sitemap.js
@@ -0,0 +1,5 @@
+module.exports = {
+  siteUrl: "https://your-domain.com", // Buraya kendi domain adınızı girin
+  generateRobotsTxt: true, // robots.txt dosyasını da oluşturmak için
+  // Diğer opsiyonel ayarlar...
+};
--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  siteUrl: "https://www.orneksiteadi.com",
-  generateRobotsTxt: true,
 };
 
 module.exports = nextConfig;
